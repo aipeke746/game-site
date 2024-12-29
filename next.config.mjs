92b2a683dd0b17ae4d7224c5c70c5d37ccf08dbb
nakatsu746 <jilptfes746@gmail.com>
2024-12-29 11:20:21 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
 const nextConfig = {
   output: "export",
+  basePath: "/game-site", // リポジトリ名を指定
+  assetPrefix: "/game-site/", // 静的アセットのパスを指定
+  images: {
+    unoptimized: true, // 必要に応じて画像最適化を無効化
+  },
 };
 
 export default nextConfig;
